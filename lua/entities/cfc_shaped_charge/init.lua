323f2163513c315919d0cda4824612c478db0502
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-03 01:51:20 +0100

    Change timer system overhaul

--- lua/entities/cfc_shaped_charge/init.lua
+++ lua/entities/cfc_shaped_charge/init.lua
@@ -2,13 +2,16 @@ AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
 AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
 include('shared.lua')
 
-    local bombHealth = 1
-    local maxBombs   = 1
-    local bombTimer  = 1
-    local blastDamage = 1
-    local blaseRange = 1
+local bombHealth
+local maxBombs
+local bombTimer
+local blastDamage
+local blaseRange
+local explodeTime
+local spawnTime
+local bombLight
  
-function ENT:Initialize()
+function ENT:Initialize()  
 
     local owner = self.Owner
     owner.plantedCharges = owner.plantedCharges or 0
@@ -19,78 +22,38 @@ function ENT:Initialize()
     bombTimer  = GetConVar("cfc_shaped_charge_timer"):GetInt()
     blastDamage  = GetConVar("cfc_shaped_charge_blastdamage"):GetInt()
     blastRange  = GetConVar("cfc_shaped_charge_blastrange"):GetInt()
-    
-    self.timeLeft = bombTimer
-    
-    if not IsValid(self.Owner) then
+        
+    if not IsValid( owner ) then
 		self:Remove()
 		return
 	end
-        
+    
 	self:SetModel( "models/weapons/w_c4_planted.mdl" )
-	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
     self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
-    self:PhysWake()
     
-    self.bombHealth = bombHealth
-    
-    local bombLight = ents.Create( "light_dynamic" )
+    bombLight = ents.Create( "light_dynamic" )
     bombLight:SetPos( self:GetPos() )
-    
     bombLight:SetKeyValue( "_light", 255, 0, 0, 200 )
     bombLight:SetKeyValue( "style", 0 )
     bombLight:SetKeyValue( "distance", 255 )
     bombLight:SetKeyValue( "brightness", 0 )
     bombLight:SetParent( self )
     bombLight:Spawn()
-
     
-    -- TODO change sound V to placing sound
+    self.bombHealth = bombHealth
+    explodeTime = CurTime() + bombTimer
+    
     self:EmitSound( "weapons/c4/c4_initiate.wav", 100, 100, 1, CHAN_WEAPON )
-    
+
     self:SetNWFloat( "bombInitiated", CurTime() )
     self:SetNWFloat( "bombDelay", bombTimer )
+    
+    spawnTime = CurTime()
+    self:bombVisualsTimer()
+end
 
-    timer.Create( "bombAlarm", 1, bombTimer, function()
-        
-        if not IsValid(self) then return end
-        
-        bombLight:SetKeyValue( "brightness", 6 )
-        timer.Simple(0.5,function()
-            if not IsValid(self) then return end
-            bombLight:SetKeyValue( "brightness", 3 )
-        end)
-        
-        self:EmitSound( "weapons/c4/c4_beep1.wav", 85, 100, 1, CHAN_STATIC )
-        
-        if timer.RepsLeft( "bombAlarm" ) == 0 then
-                            
-            local props = ents.FindAlongRay( self:GetPos(), self:GetPos() + 100 * -self:GetUp() )
-            
-            for _, prop in pairs(props) do
-                if IsValid( prop ) and prop:MapCreationID() == -1 then
-                    local shouldDestroy = hook.Call( "CFC_SWEP_Shaped_Charge", entityToDestroy )
-                    if shouldDestroy ~= false then 
-                       prop:Remove()
-                    end
-                end
-            end
-            
-            util.BlastDamage( self, self.Owner, self:GetPos(), blastRange, blastDamage )
-            
-            local effectdata = EffectData()
-            
-            effectdata:SetOrigin( self:GetPos() )
-            util.Effect("Explosion", effectdata)
-                
-            self:Remove()
-            end
-        end)
-    end
-
--- TODO find bug in OnTakeDamage, sometimes doesnt register damage
-function ENT:OnTakeDamage( dmg )
+function ENT:OnTakeDamage ( dmg )
     self.bombHealth = ( self.bombHealth ) - dmg:GetDamage()
     if self.bombHealth <= 0 then
     
@@ -115,4 +78,55 @@ function ENT:OnRemove()
     if owner.plantedCharges <= 0 then
         owner.plantedCharges = nil
     end
+end
+
+function ENT:Think()
+    if not IsValid( self ) then return end
+    
+    if explodeTime <= CurTime() then
+        self:Explode()
+    end
+end
+
+function ENT:Explode()
+    local props = ents.FindAlongRay( self:GetPos(), self:GetPos() + 100 * -self:GetUp() )
+    
+    for _, prop in pairs( props ) do
+        if IsValid( prop ) and prop:MapCreationID() == -1 then
+            local shouldDestroy = hook.Call( "CFC_SWEP_Shaped_Charge", entityToDestroy )
+            if shouldDestroy ~= false then 
+               prop:Remove()
+            end
+        end
+    end
+    
+    util.BlastDamage( self, self.Owner, self:GetPos(), blastRange, blastDamage )
+    
+    local effectdata = EffectData()
+    effectdata:SetOrigin( self:GetPos() )
+    util.Effect( "Explosion", effectdata )
+        
+    self:Remove()
+end
+
+function ENT:soundLight()
+    bombLight:SetKeyValue( "brightness", 2 )
+    timer.Simple(0.2,function()
+        if not IsValid(self) then return end
+        bombLight:SetKeyValue( "brightness", 0 )
+    end)
+    
+    self:EmitSound( "weapons/c4/c4_beep1.wav", 85, 100, 1, CHAN_STATIC )
+    self:bombVisualsTimer()
+end
+
+function ENT:bombVisualsTimer()
+    local timePassed = CurTime() - spawnTime
+    local timerDelay = math.Clamp( bombTimer / timePassed - 1, 0.13, 1 )
+    
+    timer.Simple( timerDelay, function()
+        if not IsValid(self) then return end
+        if not IsValid(self.Entity) then return end
+        self:soundLight() 
+    end)
 end
