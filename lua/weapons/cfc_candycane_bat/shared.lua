9a05fff162ec5bd67f9d4ccc9d1339b0867308eb
code-toby <wahfox8@gmail.com>
2021-11-07 19:48:00 -0500

    using base swep

--- lua/weapons/cfc_candycane_bat/shared.lua
+++ lua/weapons/cfc_candycane_bat/shared.lua
@@ -1,25 +1,165 @@
-SWEP.Author         = "{Toby}"
-SWEP.Contact        = "CFC Discord"
-SWEP.Instructions   = "left click to smak people with the candycane"
+AddCSLuaFile()
 
-SWEP.HoldType               = "melee2"
-SWEP.Spawnable              = true
+-- General info
+SWEP.Author = "Redox"
+SWEP.Purpose = "CFC Sweps."
+SWEP.PrintName = "BASESWEP"
+SWEP.Category = "CFC"
 
-SWEP.HoldType = "melee2"
-SWEP.UseHands = false
-SWEP.ViewModel = "models/weapons/v_crowbar.mdl"
-SWEP.WorldModel = "models/weapons/w_crowbar.mdl"
+-- Visuals
+SWEP.UseHands = true
+SWEP.HoldType = "knife"
+SWEP.DrawCrosshair = true
+SWEP.IconOverride = ""
+SWEP.ViewModel		= "models/weapons/v_knife_t.mdl"
+SWEP.WorldModel		= "models/weapons/w_knife_t.mdl"
+SWEP.Slot = 3
+SWEP.SlotPos = 1
+SWEP.DrawWeaponInfoBox = true
 
-SWEP.swingSound = "weapons/bat_draw_swoosh2.wav"
-SWEP.hitSound = "weapons/demo_charge_hit_world2.wav"
+-- Sounds
+SWEP.HitSounds = {
+    "weapons/knife/knife_hit1.wav",
+    "weapons/knife/knife_hit2.wav",
+    "weapons/knife/knife_hit3.wav",
+    "weapons/knife/knife_hit4.wav"
+}
+SWEP.BackStabSound = "weapons/knife/knife_stab.wav"
+SWEP.HitWorldSound = "weapons/knife/knife_hitwall1.wav"
+SWEP.SwingSound = "weapons/knife/knife_swing_miss1.wav"
+SWEP.DeploySound = "weapons/knife/knife_deploy.wav"
+SWEP.HolsterSound = ""
 
-SWEP.Primary.ClipSize       = -1
-SWEP.Primary.Delay          = 0.4
-SWEP.Primary.DefaultClip    = -1
-SWEP.Primary.Automatic      = true
-SWEP.Primary.Ammo           = "none"
+-- Functionals
+SWEP.Slot = 1
+SWEP.SlotPos = 1
+SWEP.Spawnable = false
+SWEP.AdminSpawnable = false
+SWEP.AdminOnly = false
 
-SWEP.Secondary.ClipSize     = -1
-SWEP.Secondary.DefaultClip  = -1
-SWEP.Secondary.Automatic    = false
-SWEP.Secondary.Ammo         = "none"
+-- Ammo and such
+-- Primary
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.DefaultClip = 1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = ""
+SWEP.Primary.Automatic = true
+
+-- Secondary
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = true
+SWEP.Secondary.Ammo = ""
+
+-- Stats (damage and such)
+SWEP.PrimaryDamage = 25
+SWEP.SecondaryDamage = 50
+SWEP.BackStabMultiplier = 3 -- The damage multiplier on backstabs
+SWEP.DamageRandom = 4 --Random +- added to the damage to make it more organic
+
+SWEP.ShouldBackstab = true -- If the swep is able to deal double damage on backstabs
+SWEP.Range = 70 -- The range of the melee attack
+
+SWEP.PrimaryFireRate = 0.5 -- Interval the pimary attack can be used
+SWEP.SecondaryFireRate = 1 -- Interval the secondary attack can be used
+SWEP.BackStabFireRateMult = 1.5 -- The interval multiplier after backstabbing someone
+
+function SWEP:Initialize()
+    self:SetHoldType( self.HoldType )
+    return true
+end
+
+function SWEP:Deploy()
+    self:EmitSound( self.DeploySound )
+    return true
+end
+
+function SWEP:Holster()
+    self:EmitSound( self.HolsterSound )
+    return true
+end
+
+function SWEP:EntityFaceBack( ent )
+    local angle = self:GetOwner():GetAngles().y - ent:GetAngles().y
+    if angle < -180 then angle = 360 + angle end
+    if angle <= 90 and angle >= -90 then return true end
+    return false
+end
+
+function SWEP:DoAttack( primary )
+    local ply = self:GetOwner()
+    local curTime = CurTime()
+    local traceEndPos = ply:EyePos() + ply:EyeAngles():Forward() * self.Range
+
+    local tr = util.TraceLine({
+        start = ply:EyePos(),
+        endpos = traceEndPos,
+        filter = ply,
+    })
+
+    local ent = tr.Entity
+
+    if ent:IsWorld() then
+        self:SendWeaponAnim( ACT_VM_MISSCENTER )
+        self:SetNextPrimaryFire( curTime + self.PrimaryFireRate )
+        self:EmitSound( self.HitWorldSound )
+        return true
+    end
+
+    if not IsValid( ent ) then
+        self:SendWeaponAnim( ACT_VM_MISSCENTER )
+        self:SetNextPrimaryFire( curTime + self.PrimaryFireRate )
+        self:EmitSound( self.SwingSound )
+        return true
+    end
+
+    local isBackstab = self:EntityFaceBack( ent )
+    local force = ply:GetAimVector():GetNormalized() * 300 * cvars.Number("phys_pushscale", 1)
+    local damageInfo = DamageInfo()
+
+    damageInfo:SetAttacker( ply )
+    damageInfo:SetInflictor( self )
+    damageInfo:SetDamageType( bit.bor( DMG_SLASH , DMG_NEVERGIB ) )
+    damageInfo:SetDamageForce( force )
+    damageInfo:SetDamagePosition( traceEndPos )
+
+    local damageMultiplier = 1
+    local firerateMultiplier = 1
+    if isBackstab then
+        damageMultiplier = self.BackStabMultiplier
+        firerateMultiplier = self.BackStabFireRateMult
+    end
+
+    if primary then
+        damageInfo:SetDamage( ( math.random( -self.DamageRandom, self.DamageRandom ) + self.PrimaryDamage ) * damageMultiplier )
+        ent:DispatchTraceAttack( damageInfo, tr, ply:GetAimVector() )
+
+        self:SetNextPrimaryFire( curTime + self.PrimaryFireRate * firerateMultiplier )
+
+        self:EmitSound( self.HitSounds[math.random( #self.HitSounds )] )
+    else
+        damageInfo:SetDamage( ( math.random( -self.DamageRandom, self.DamageRandom ) + self.SecondaryDamage ) * damageMultiplier )
+        ent:DispatchTraceAttack( damageInfo, tr, ply:GetAimVector() )
+
+        self:SetNextPrimaryFire( curTime + self.SecondaryFireRate * firerateMultiplier )
+
+        self:EmitSound( self.BackStabSound )
+    end
+
+    self:SetAnimation( PLAYER_ATTACK1 )
+    self:SendWeaponAnim( ACT_VM_HITCENTER )
+    return true
+end
+
+function SWEP:PrimaryAttack()
+    self:DoAttack( true )
+end
+
+function SWEP:SecondaryAttack()
+    if CurTime() < self:GetNextPrimaryFire() then return end -- Both use primary fire to prevent prim and sec fire at the same time
+    self:DoAttack( false )
+end
+
+function SWEP:Reload()
+    return
+end
